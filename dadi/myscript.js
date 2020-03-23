25cbb91dd76492a2056a74df695924e6a216fb28
Emanuele <em.zaccaria@gmail.com>
2020-03-23 17:33:57 +0100

    inizio dadi

---
+++ dadi/myscript.js
@@ -0,0 +1,12 @@
+// Gioco dei dadi, chi fa di più vince.
+
+// creiamo il dado
+var dado;
+dado = [1,2,3,4,5,6]
+
+// Un utente tira il primo dado ed avrà un risultato
+
+
+// Un altro utente tira un altro dado ed avrà un risulato
+
+// i due risultati vengono cofrontati e si decreta il vincitore
